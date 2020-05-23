19848e3e11ec9a872ce63a1305ace3edafd0d290
syuheifujita <syuheifujita0117@gmail.com>
2020-05-23 13:36:43 +0900

    add SignUPActivity

--- app/src/main/java/com/syuheifujita/android_usedfirebase_app/IntroActivity.kt
+++ app/src/main/java/com/syuheifujita/android_usedfirebase_app/IntroActivity.kt
@@ -1,8 +1,10 @@
 package com.syuheifujita.android_usedfirebase_app
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
 
@@ -14,5 +16,9 @@ class IntroActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
