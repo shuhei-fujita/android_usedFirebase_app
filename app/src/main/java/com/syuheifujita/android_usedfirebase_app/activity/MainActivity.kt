c6cf0a9c7da82942396772feba8ac4216e855d19
syuheifujita <syuheifujita0117@gmail.com>
2020-06-03 01:06:14 +0900

    Glideライブラリを使って，Firebaseのdatabaseからuser_imageをインポートできた

--- app/src/main/java/com/syuheifujita/android_usedfirebase_app/activity/MainActivity.kt
+++ app/src/main/java/com/syuheifujita/android_usedfirebase_app/activity/MainActivity.kt
@@ -1,13 +1,97 @@
 package com.syuheifujita.android_usedfirebase_app.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.syuheifujita.android_usedfirebase_app.R
+import com.syuheifujita.android_usedfirebase_app.firebase.FirestoreClass
+import com.syuheifujita.android_usedfirebase_app.model.UserModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpActionBar()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        FirestoreClass().signInUser(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+        }
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(tb_main_activity)
+        tb_main_activity.setNavigationIcon(R.drawable.ic_acition_navigation_menu)
+
+        tb_main_activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_my_profile -> {
+                Toast.makeText(this, " ", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                startActivity(
+                    Intent(this, IntroActivity::class.java)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
+                finish()
+
+                Toast.makeText(this, " ", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+
+        return true
+    }
+
+    fun updateNavigationUserDetails(user: UserModel) {
+        // The instance of the header view of the navigation view.
+        val headerView = nav_view.getHeaderView(0)
+
+        // The instance of the user image of the navigation view.
+        val navUserImage = headerView.findViewById<ImageView>(R.id.nav_user_image)
+
+        // Load the user image in the ImageView.
+        Glide
+            .with(this@MainActivity)
+            .load(user.image) // URL of the image
+            .centerCrop() // Scale type of the image.
+            .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+            .into(navUserImage) // the view in which the image will be loaded.
+
+        tv_username.text = user.name
     }
 }
--- app/src/main/java/com/syuheifujita/android_usedfirebase_app/firebase/FirestoreClass.kt
+++ app/src/main/java/com/syuheifujita/android_usedfirebase_app/firebase/FirestoreClass.kt
@@ -1,10 +1,11 @@
 package com.syuheifujita.android_usedfirebase_app.firebase
 
+import android.app.Activity
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.auth.User
+import com.syuheifujita.android_usedfirebase_app.activity.MainActivity
 import com.syuheifujita.android_usedfirebase_app.activity.SignInActivity
 import com.syuheifujita.android_usedfirebase_app.activity.SignUpActivity
 import com.syuheifujita.android_usedfirebase_app.model.UserModel
@@ -23,17 +24,41 @@ class FirestoreClass {
             }
     }
 
-    fun signInUser(activity: SignInActivity) {
+    fun updateNavigationUserDetails(user: UserModel) {
+
+    }
+
+    fun signInUser(activity: Activity) {
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserId())
             .get()
             .addOnSuccessListener { document ->
                 val loggedInUser = document.toObject(UserModel::class.java)!!
-                if(loggedInUser != null)
-                    activity.signInSuccess(loggedInUser)
-            }.addOnFailureListener {
-                e ->
-                Log.e(activity.javaClass.simpleName, "Error")
+
+                when(activity) {
+                    is SignInActivity -> {
+                        activity.signInSuccess(loggedInUser)
+                    }
+                    is MainActivity -> {
+                        activity.updateNavigationUserDetails(loggedInUser)
+                    }
+                }
+            }.addOnFailureListener {e ->
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                    is MainActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                    // END
+                }
+                // END
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error while getting loggedIn user details",
+                    e
+                )
             }
     }
 
